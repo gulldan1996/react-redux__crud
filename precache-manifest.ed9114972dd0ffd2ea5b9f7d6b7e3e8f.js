bb6721eec06b0f7542abe2d70942c055cc037219
Olegus <gulldan1996@gmail.com>
2019-11-14 11:16:50 +0200

    Updates

---
+++ precache-manifest.ed9114972dd0ffd2ea5b9f7d6b7e3e8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "393c55f792b6f97c862552f02dbeaf7c",
+    "url": "/react-redux-crud/index.html"
+  },
+  {
+    "revision": "2d7da1b7cd9c138a7098",
+    "url": "/react-redux-crud/static/css/main.673c4b5e.chunk.css"
+  },
+  {
+    "revision": "4395b3b566faaca403c2",
+    "url": "/react-redux-crud/static/js/2.a1c04535.chunk.js"
+  },
+  {
+    "revision": "2d7da1b7cd9c138a7098",
+    "url": "/react-redux-crud/static/js/main.66b4f13c.chunk.js"
+  },
+  {
+    "revision": "960e7c33416bfb34dde7",
+    "url": "/react-redux-crud/static/js/runtime-main.3f8bbc09.js"
+  }
+]);
